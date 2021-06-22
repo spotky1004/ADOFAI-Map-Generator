94d2d1937ce646b0cd27459d120320ba5c331235
Spotky1004 <spotky1004@gmail.com>
2021-06-22 20:00:46 +0900

    non-linear map generation (2)

--- mapGenerator.js
+++ mapGenerator.js
@@ -5,18 +5,16 @@ import ADOFAI from "./ADOFAI-WebModule-master/ADOFAI-WebModule.js";
 
 
 // math math
-function gcd(a, b) {
-    if (a == 0)
-        return b;
-
-    while (b != 0) {
-        if (a > b)
-            a = a - b;
-        else
-            b = b - a;
+function gcd(a,b) {
+    a = Math.abs(a);
+    b = Math.abs(b);
+    if (b > a) {var temp = a; a = b; b = temp;}
+    while (true) {
+        if (b == 0) return a;
+        a %= b;
+        if (a == 0) return b;
+        b %= a;
     }
-
-    return a;
 }
 function calcNumerator(a) {
     let loop = 1;
@@ -46,30 +44,76 @@ function generateMap(musicData) {
     
     let bpmMap = [];
     for (let i = 1, l = musicData.length; i < l; i++) {
-        bpmMap.push(60*1000/(musicData[i]-musicData[i-1]));
+        bpmMap.push( 60*1000/(musicData[i]-musicData[i-1]) );
     }
-    
 
-    const intBpm = bpmMap.filter(Number.isInteger);
-    let baseBpm = gcd(Math.min(...intBpm), Math.max(...intBpm));
-    let isBaseBpmVaild = intBpm.filter(e => Number.isInteger(e/baseBpm) && e/baseBpm < 24).length === intBpm.length;
+    let counter = {};
+    for (let i = 0, l = bpmMap.length; i < l; i++) {
+        const tileBpm = bpmMap[i];
+        if (!counter[tileBpm]) counter[tileBpm] = 0;
+        counter[tileBpm]++;
+    }
+
+    let counterPer = {};
+    for (const bpm in counter) {
+        counterPer[bpm] = counter[bpm]/bpmMap.length;
+    }
+
+    counterPer = Object.entries(counterPer);
+    const maxCountPer = Math.max(...counterPer.map(e => e[1]));
+    counterPer = counterPer.filter(e => e[1] > maxCountPer/12);
+    const filteredBpmList = counterPer.map(e => Number(e[0]));
+
+    const primeFactor24 = [1, 2, 3, 4, 6, 8, 12, 24];
+    let samples = [];
+    for (let i = 0, l = filteredBpmList.length; i < l; i++) {
+        const samplingBaseBpm = filteredBpmList[i];
+        for (const factor of primeFactor24) {
+            const samplingBpm = samplingBaseBpm/factor;
+            const samplingBpmList = primeFactor24.map(e => (samplingBpm*12/e).toFixed(7));
+            let accury = 0;
+            let canChangePaths = [];
+            let pathIncrements = [];
+            bpmMap.some((tileBpm, i) => {
+                if (samplingBpmList.includes(tileBpm.toFixed(7))) {
+                    accury += 1/bpmMap.length;
+                    canChangePaths.push(i);
+                    pathIncrements.push(samplingBpmList.findIndex(e => e === tileBpm.toFixed(7)));
+                }
+            });
+            samples.push({
+                bpm: samplingBpm,
+                canChangePaths: canChangePaths,
+                pathIncrements: pathIncrements,
+                accury: accury
+            });
+        }
+    }
+
+    const bestSample = samples.sort((a, b) => b.accury - a.accury)[0];
+    console.log(bestSample);
 
     
-    if (!document.getElementsByName("linearBool")[0].checked && isBaseBpmVaild && baseBpm !== 1) {
-        map.settings.bpm = baseBpm;
+    if (!document.getElementsByName("linearBool")[0].checked && bestSample.bpm > 0) {
+        map.settings.bpm = bestSample.bpm;
 
         let prevPath = 12-1;
         let pathIdx = 1;
         let eventIdx = 0;
 
+        let changeablePathIdx = 0;
+
         for (let i = 0, l = bpmMap.length; i < l; i++) {
-            const tileBpm = bpmMap[i];
-            const baseFraction = tileBpm/baseBpm;
-            if (Number.isInteger(baseFraction)) {
-                prevPath += 12+baseFraction;
+            if (bestSample.canChangePaths[changeablePathIdx] === i) {
+                prevPath += Math.floor(bpmMap[i]/bestSample.bpm*12);
                 map.pathData.push( new ADOFAI.PathData(path[prevPath%24]) );
+
+                changeablePathIdx++;
+
                 pathIdx++;
             } else {
+                const baseFraction = bpmMap[i]/bestSample.bpm;
+
                 map.pathData.push( new ADOFAI.PathData(path[prevPath%24]) );
 
                 if (!map.actions.length || map.actions[map.actions.length-1].floor !== pathIdx) {
@@ -77,11 +121,11 @@ function generateMap(musicData) {
                     map.actions[eventIdx].eventValue.speedType = "Multiplier";
                     eventIdx++;
                 }
-                map.actions[eventIdx-1].eventValue.bpmMultiplier *= tileBpm/baseBpm;
+                map.actions[eventIdx-1].eventValue.bpmMultiplier *= baseFraction;
 
                 map.actions.push( new ADOFAI.Action(pathIdx+1, "SetSpeed") );
                 map.actions[eventIdx].eventValue.speedType = "Multiplier";
-                map.actions[eventIdx].eventValue.bpmMultiplier /= tileBpm/baseBpm;
+                map.actions[eventIdx].eventValue.bpmMultiplier /= baseFraction;
 
                 pathIdx++;
                 eventIdx++;
@@ -101,7 +145,7 @@ function generateMap(musicData) {
     
     document.getElementById("mapOutput").innerHTML = map.Export();
     document.getElementById("configWarper").style.display = "none";
-    console.log(map, baseBpm, isBaseBpmVaild);
+    console.log(map);
 }
 
 window.calcNumerator = calcNumerator;
